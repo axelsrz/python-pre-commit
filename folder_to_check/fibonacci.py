b064aa2f32cac6eb2c8ac290fc7aec9699c4ebb5
aliandi <mica.andini@gmail.com>
2019-06-21 16:36:06 -0300

    Test

--- folder_to_check/fibonacci.py
+++ folder_to_check/fibonacci.py
@@ -1,16 +1,14 @@
 def helloWorld():
     print("HelloWorld")
-
-
 def fibonacci(n):
-    if n < 0:
+    if n<0:
         print("Incorrect input")
-    elif n ==1:
+    elif n==1:
         return 0
-    elif n == 2:
+    elif n==2:
         return 1
     else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+        return fibonacci(n-1)+fibonacci(n-2)
 
 
 print(fibonacci(9))
